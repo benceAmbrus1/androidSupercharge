c61d4b25fa5d1882237d09816e3400dc6c4f04ee
benceAmbrus1 <o.g.bence@gmail.com>
2018-09-11 09:52:40 +0200

    basic models mostly done

--- app/src/main/java/com/example/ogben/androidsupercharge/model/Movie.java
+++ app/src/main/java/com/example/ogben/androidsupercharge/model/Movie.java
@@ -1,4 +1,33 @@
 package com.example.ogben.androidsupercharge.model;
 
+import com.google.gson.annotations.SerializedName;
+
 public class Movie {
+
+    @SerializedName("poster_path")
+    private String posterPath;
+
+    @SerializedName("release_date")
+    private String releaseDate;
+
+    @SerializedName("title")
+    private String title;
+
+    public Movie(String posterPath, String releaseDate, String title) {
+        this.posterPath = posterPath;
+        this.releaseDate = releaseDate;
+        this.title = title;
+    }
+
+    public String getPosterPath() {
+        return posterPath;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public String getTitle() {
+        return title;
+    }
 }
--- app/src/main/java/com/example/ogben/androidsupercharge/model/MoviePage.java
+++ app/src/main/java/com/example/ogben/androidsupercharge/model/MoviePage.java
@@ -1,4 +1,15 @@
 package com.example.ogben.androidsupercharge.model;
 
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
 public class MoviePage {
+
+    @SerializedName("results")
+    private List<Movie> results;
+
+    public List<Movie> getResults() {
+        return results;
+    }
 }
