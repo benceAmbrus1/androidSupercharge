d6b99e13944b4dc2f52ae6565bf3c7159c5b3b45
benceAmbrus1 <o.g.bence@gmail.com>
2018-09-11 10:08:28 +0200

    Main activity connet api on button click, and get movies

--- app/src/main/java/com/example/ogben/androidsupercharge/activity/MainActivity.java
+++ app/src/main/java/com/example/ogben/androidsupercharge/activity/MainActivity.java
@@ -2,14 +2,70 @@ package com.example.ogben.androidsupercharge.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
 
 import com.example.ogben.androidsupercharge.R;
+import com.example.ogben.androidsupercharge.model.Movie;
+import com.example.ogben.androidsupercharge.model.MoviePage;
+import com.example.ogben.androidsupercharge.service.MovieAPIService;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    public static final String BASEURL = "https://api.themoviedb.org/3/";
+    private final static String API_KEY = "92290e0f78d9487afe3e01292483c2e3";
+    private ListView mainListView;
+    private Button loadButton;
+    private ArrayAdapter<Movie> adapter;
+    private static Retrofit retrofit;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mainListView = findViewById(R.id.main_list_view);
+        loadButton = findViewById(R.id.load_movies_button);
+
+        loadButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                joinAPIandGetMovies();
+            }
+        });
+    }
+
+    private void joinAPIandGetMovies(){
+        if (retrofit == null) {
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASEURL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+
+        MovieAPIService movieService = retrofit.create(MovieAPIService.class);
+        Call<MoviePage> call = movieService.getTopRatedMovies(API_KEY);
+        call.enqueue(new Callback<MoviePage>() {
+            @Override
+            public void onResponse(Call<MoviePage> call, Response<MoviePage> response) {
+                List<Movie> movies = response.body().getResults();
+            }
+            @Override
+            public void onFailure(Call<MoviePage> call, Throwable throwable) {
+                Log.e(MainActivity.class.getSimpleName(), throwable.toString());
+            }
+        });
     }
 }
