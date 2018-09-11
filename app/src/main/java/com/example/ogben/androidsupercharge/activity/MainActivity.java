3cb61013a0893456d6e6ef6d61074d03660222cd
benceAmbrus1 <o.g.bence@gmail.com>
2018-09-11 09:34:10 +0200

    basic structure

--- app/src/main/java/com/example/ogben/androidsupercharge/activity/MainActivity.java
+++ app/src/main/java/com/example/ogben/androidsupercharge/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.ogben.androidsupercharge;
+package com.example.ogben.androidsupercharge.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.ogben.androidsupercharge.R;
 
 public class MainActivity extends AppCompatActivity {
 
