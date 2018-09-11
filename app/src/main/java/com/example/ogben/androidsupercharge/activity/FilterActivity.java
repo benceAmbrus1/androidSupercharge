866710207523c34c30a23718d3fdda4782b5f4e9
benceAmbrus1 <o.g.bence@gmail.com>
2018-09-11 12:31:01 +0200

    last commit, add spinner to filter_layout

--- app/src/main/java/com/example/ogben/androidsupercharge/activity/FilterActivity.java
+++ app/src/main/java/com/example/ogben/androidsupercharge/activity/FilterActivity.java
@@ -4,13 +4,16 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 
 import com.example.ogben.androidsupercharge.R;
 
 public class FilterActivity extends AppCompatActivity {
 
     private Button backButton;
+    private Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,5 +28,11 @@ public class FilterActivity extends AppCompatActivity {
                 startActivity(mainIntent);
             }
         });
+
+        spinner = findViewById(R.id.filter_spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.filter_type, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
     }
 }
